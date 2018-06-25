6cd5bc5ccfab61478946e6ceec93526095217eff
Alex Charles <alexjob44@gmail.com>
2018-06-25 14:45:59 -0600

    Message

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var conString = 'postgres://jmjpstzyyzexxe:127c8c9e3b14656d5e45ae9fc2904a79c958c
 
 //Users
 //get all users
-router.get('/users', function(req, res, next) {
+router.get('/vehicle', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
       return console.error('error fetching client from pool', err);
