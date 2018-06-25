4bc0a0679890649a2da63bc790815f61b7d3c03f
Alex Charles <alexjob44@gmail.com>
2018-06-25 14:58:02 -0600

    Message

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ var conString = 'postgres://jmjpstzyyzexxe:127c8c9e3b14656d5e45ae9fc2904a79c958c
 //Users
 //get all users
 router.get('/vehicle', function(req, res, next) {
+  var html = "<div id='email_window'><h2>Email Share</h2><div>";
+  $("#data").html(html);
   pg.connect(conString, function(err, client, done) {
     if (err) {
       return console.error('error fetching client from pool', err);
