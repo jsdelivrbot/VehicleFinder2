abffef2a80515afd38b82d6019324a5e9e13fbab
Alex Charles <alexjob44@gmail.com>
2018-06-25 14:44:57 -0600

    Message

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/users', function(req, res, next) {
       if (err) {
         return console.error('error running query', err);
       }
-      res.send(result.rows[1]);
+      res.send(result.rows[1].name); //Returns {"name":"test_car"}
     });
   });
 });
