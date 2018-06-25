b2caffee049d169a0dc6465318a2e595b67b1f95
Alex Charles <alexjob44@gmail.com>
2018-06-25 16:42:29 -0600

    Message

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,7 @@ var conString = 'postgres://jmjpstzyyzexxe:127c8c9e3b14656d5e45ae9fc2904a79c958c
 
 //Users
 //get all users
-//router.get('/vehicle', function(req, res, next) {
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+router.get('/vehicle', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
       return console.error('error fetching client from pool', err);
