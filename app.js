f46b40042494fe392aa097380c28bb5eb14fda7b
Alex Charles <alexjob44@gmail.com>
2018-06-25 13:36:12 -0600

    Message

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+require('dotenv').load();
+var routes = require('./routes/index');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var conString = process.env.DATABASE_URL;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
