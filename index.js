7483249cb945da8feb54b3c4628b722073a57031
Alex Charles <alexjob44@gmail.com>
2018-06-27 17:32:41 -0600

    Message

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ var express = require('express');
 var app = express();
 
 const { Pool } = require("pg");
-
-
 const connectionString = "postgres://kqbjayaoloktbi:53ca815c90f87a7674987395b394905de14898d1ae59f091d9dd8ab1ca10173c@ec2-54-83-12-150.compute-1.amazonaws.com:5432/dbvslumrik88hn";
-
 const pool = new Pool({connectionString: connectionString});
-
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
 app.get('/getVehicle', function(request, response) {
 	getVehicle(request, response);
 });
@@ -21,7 +18,8 @@ app.listen(app.get('port'), function() {
 
 function getVehicle(request, response) {
 	var id = request.query.id;
-	getVehicleFromDb(id, function(error, result) {
+	var type = request.query.type;
+	getVehicleFromDb(id, type, function(error, result) {
 		if (error || result == null || result.length != 1) {
 			response.status(500).json({success: false, data: error});
 		} else {
@@ -31,10 +29,10 @@ function getVehicle(request, response) {
 	});
 }
 
-function getVehicleFromDb(id, callback) {
+function getVehicleFromDb(id, type, callback) {
 	console.log("Getting vehicle from DB with id: " + id);
-	var sql = "SELECT * FROM vehicle WHERE id = $1::int";
-	var params = [id];
+	var sql = "SELECT * FROM vehicle WHERE fuel_economy = $1::int and type = $2::int";
+	var params = [id, type];
 
 
 	pool.query(sql, params, function(err, result) {
