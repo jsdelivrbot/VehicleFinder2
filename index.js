8565a1f82c185b056f54fd2ebb216033cbb56a00
Alex Charles <alexjob44@gmail.com>
2018-06-27 16:49:22 -0600

    Message

--- index.js
+++ index.js
@@ -1,91 +1,51 @@
 var express = require('express');
 var app = express();
 
+const { Pool } = require("pg");
 
-// Following the "Single query" approach from: https://node-postgres.com/features/pooling#single-query
 
-const { Pool } = require("pg"); // This is the postgres database connection module.
+const connectionString = "postgres://kqbjayaoloktbi:53ca815c90f87a7674987395b394905de14898d1ae59f091d9dd8ab1ca10173c@ec2-54-83-12-150.compute-1.amazonaws.com:5432/dbvslumrik88hn";
 
-// This says to use the connection string from the environment variable, if it is there,
-// otherwise, it will use a connection string that refers to a local postgres DB
-const connectionString = "postgres://ldmdzzhecofrzg:a93dfe93169e13887b35d0dfd97de00b6f0acc285777c9baed3ea1c8ca63245@ec2-54-83-12-150.compute-1.amazonaws.com:5432/d67udl2mh0iphv";
-
-// Establish a new connection to the data source specified the connection string.
 const pool = new Pool({connectionString: connectionString});
 
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
-// This says that we want the function "getPerson" below to handle
-// any requests that come to the /getPerson endpoint
-app.get('/getPerson', function(request, response) {
+app.get('/getVehicle', function(request, response) {
 	getPerson(request, response);
 });
 
-// Start the server running
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
-// This function handles requests to the /getPerson endpoint
-// it expects to have an id on the query string, such as: http://localhost:5000/getPerson?id=1
-function getPerson(request, response) {
-	// First get the person's id
+function getVehicle(request, response) {
 	var id = request.query.id;
-
-	// TODO: We should really check here for a valid id before continuing on...
-
-	// use a helper function to query the DB, and provide a callback for when it's done
-	getPersonFromDb(id, function(error, result) {
-		// This is the callback function that will be called when the DB is done.
-		// The job here is just to send it back.
-
-		// Make sure we got a row with the person, then prepare JSON to send back
+	getVehicleFromDb(id, function(error, result) {
 		if (error || result == null || result.length != 1) {
 			response.status(500).json({success: false, data: error});
 		} else {
-			var person = result[0];
+			var vehicle = result[0];
 			response.status(200).json(result[0]);
 		}
 	});
 }
 
-// This function gets a person from the DB.
-// By separating this out from the handler above, we can keep our model
-// logic (this function) separate from our controller logic (the getPerson function)
-function getPersonFromDb(id, callback) {
-	console.log("Getting person from DB with id: " + id);
-
-	// Set up the SQL that we will use for our query. Note that we can make
-	// use of parameter placeholders just like with PHP's PDO.
-	var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
-
-	// We now set up an array of all the parameters we will pass to fill the
-	// placeholder spots we left in the query.
+function getVehicleFromDb(id, callback) {
+	console.log("Getting vehicle from DB with id: " + id);
+	var sql = "SELECT * FROM vehicle WHERE id = $1::int";
 	var params = [id];
 
-	// This runs the query, and then calls the provided anonymous callback function
-	// with the results.
+
 	pool.query(sql, params, function(err, result) {
-		// If an error occurred...
 		if (err) {
 			console.log("Error in query: ")
 			console.log(err);
 			callback(err, null);
 		}
 
-		// Log this to the console for debugging purposes.
 		console.log("Found result: " + JSON.stringify(result.rows));
-
-
-		// When someone else called this function, they supplied the function
-		// they wanted called when we were all done. Call that function now
-		// and pass it the results.
-
-		// (The first parameter is the error variable, so we will pass null.)
 		callback(null, result.rows);
 	});
 
-} // end of getPersonFromDb
+}
