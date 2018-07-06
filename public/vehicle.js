7b9ae03fc4ac5acd96121b3c30fbb8ef8a4b3ab0
Alex Charles <alexjob44@gmail.com>
2018-07-06 15:39:15 -0600

    Message

--- public/vehicle.js
+++ public/vehicle.js
@@ -29,6 +29,11 @@ function search(){
 function handleResultList(result){
   console.log("Back from AJAX with result:");
   var obj = JSON.parse(result);
-  console.log(obj[0]);
-  console.log(obj[0].name);
+  var resultList = $("#ulResults");
+  var i = 0;
+  while (obj[i])
+  {
+      resultList.append(obj[i].name);
+      i++;
+  }
 }
